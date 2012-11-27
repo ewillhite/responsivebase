aeecb0cac66d6dd3e0af8184144962c5b24a72da
Evan Willhite <ewillhite@centresource.com>
2012-11-27 15:03:15 -0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// All jQuery/javascript run through the script file must be wrapped in the function in this file.  This is also true for scripts run in Console or Firebug.
+(function ($) {
+
+$(function() {
+
+// Apply responsive widths to videos via FitVids
+$('.region').fitVids();
+
+});
+
+})(jQuery);
